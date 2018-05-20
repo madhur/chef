34077964c51bd83788ca3a623ce7444d1671cb3f
vagrant <vagrant@localhost.localdomain>
2018-05-20 14:01:00 +0000

    initial commit

---
+++ workstation/recipes/setup.rb
@@ -0,0 +1,20 @@
+package 'tree' do
+	action  :install
+end
+
+package 'git' do
+	action :install
+end
+
+package 'ntp' 
+
+file '/etc/motd' do
+	content 'This server is the property of TechnoTrainer'
+	action :create
+	owner 'root'
+	group 'root'
+end
+
+service 'ntpd' do
+	action [ :enable, :start ]
+end
